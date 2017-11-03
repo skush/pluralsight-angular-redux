aa24d2d09f4afda5506fa9dd9a4aa1034ed54a5a
Sergii Kushnir <skush@users.noreply.github.com>
2017-11-03 15:10:55 +0200

    basic integration followed along

---
+++ src/app/store/reducer.ts
@@ -0,0 +1,29 @@
+import { Course } from '../courses/course';
+import { IAppState } from './IAppState';
+
+const courses = [];
+
+const initialState: IAppState = {
+    courses :  [
+      {
+        "id": 1,
+        "name": "Building Apps with React",
+        "topic": "ReactJS"
+      },
+      {
+        "id": 2,
+        "name": "Building Apps with Angular",
+        "topic": "AngularJS"
+      },
+      {
+        "id":3,
+        "name": "Building Apps with Angular and Redux",
+        "topic": "Angular and Redux"
+      }
+    ]//,
+    ////filteredCourses: courses
+};
+
+export function reducer(state = initialState, action) {
+  return state;
+}
